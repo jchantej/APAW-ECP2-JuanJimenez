145b8297f4f2b2568dc0057892dcffdf8e66d451
Juan Pablo Jimenez <jchantej@gmail.com>
2017-10-07 18:24:15 +0200

    test de funcionalidad obtener auhtor por  ID

--- src/main/java/arquitectura/api/resources/exceptions/AuthorFieldInvalidException.java
+++ src/main/java/arquitectura/api/resources/exceptions/AuthorFieldInvalidException.java
@@ -3,7 +3,7 @@ package arquitectura.api.resources.exceptions;
 public class AuthorFieldInvalidException extends Exception {
     private static final long serialVersionUID = -642045799876625537L;
 
-    public static final String DESCRIPTION = "Nombre del tema no encontrado";
+    public static final String DESCRIPTION = "Nombre del author no encontrado";
 
     public AuthorFieldInvalidException(String detail) {
         super(DESCRIPTION + ". " + detail);
--- src/test/java/arquitectura/AuthorResourceFunctionalTesting.java
+++ src/test/java/arquitectura/AuthorResourceFunctionalTesting.java
@@ -1,5 +1,7 @@
 package arquitectura;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import arquitectura.api.daos.DaoFactory;
@@ -10,6 +12,7 @@ import arquitectura.http.HttpException;
 import arquitectura.http.HttpMethod;
 import arquitectura.http.HttpRequest;
 import arquitectura.http.HttpRequestBuilder;
+
 
 public class AuthorResourceFunctionalTesting {
 
@@ -37,6 +40,22 @@ public class AuthorResourceFunctionalTesting {
     @Test(expected = HttpException.class)
     public void testCreateWithoutAuthorName() {
         HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(AuthorResource.AUTHOR).build();
+        new HttpClientService().httpRequest(request);
+    }
+    
+    @Test
+    public void testReadAuthor() {
+        this.createTheme();
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(AuthorResource.AUTHOR).path("/{id}")
+                .expandPath("1").build();
+      //  assertEquals("{\"id\":1,\"name\":\"Pablo Jimenez\"}", new HttpClientService().httpRequest(request).getBody());
+
+    }
+    
+    @Test(expected = HttpException.class)
+    public void testAuthorIdNotFound() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(AuthorResource.AUTHOR).path("/{id}")
+                .expandPath("1").build();
         new HttpClientService().httpRequest(request);
     }
 
