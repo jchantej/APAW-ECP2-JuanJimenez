d462d14d7a37fbc3b7d05742877bd401b23f89c0
Juan Pablo Jimenez <jchantej@gmail.com>
2017-10-08 17:45:18 +0200

    #3 controllador listar authors

--- src/test/java/arquitectura/api/controllers/AuthorControllerIT.java
+++ src/test/java/arquitectura/api/controllers/AuthorControllerIT.java
@@ -1,13 +1,10 @@
 package arquitectura.api.controllers;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import arquitectura.api.daos.DaoFactory;
 import arquitectura.api.daos.memory.DaoMemoryFactory;
-
 
 
 public class AuthorControllerIT {
@@ -18,7 +15,7 @@ public class AuthorControllerIT {
     public void before() {
         DaoFactory.setFactory(new DaoMemoryFactory());
         authorController = new AuthorController();
-   
+
     }
 
     @Test
@@ -29,6 +26,21 @@ public class AuthorControllerIT {
     @Test
     public void testReadAuthor() {
         authorController.createAuthor("Pablo Jimenez");
-       assertEquals("Pablo Jimenez",authorController.readAuthor(1).get().getAuthorName());
+        assertEquals("Pablo Jimenez", authorController.readAuthor(1).get().getAuthorName());
     }
+
+    @Test
+    public void testAuthorList() {
+        authorController.createAuthor("Pablo Jimenez");
+        authorController.createAuthor("Juan Jimenez");
+        assertEquals(2, authorController.authorList().size());
+        assertEquals(1, authorController.authorList().get(0).getAuthorId());
+        assertEquals("Pablo Jimenez", authorController.authorList().get(0).getAuthorName());
+        assertEquals("Español", authorController.authorList().get(0).getAuthorLanguage());
+        
+        assertEquals(2, authorController.authorList().get(1).getAuthorId());
+        assertEquals("Juan Jimenez", authorController.authorList().get(1).getAuthorName());
+        assertEquals("Español", authorController.authorList().get(1).getAuthorLanguage());
+    }
+
 }
